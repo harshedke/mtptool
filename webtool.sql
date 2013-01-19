6be55d3f652387305aaadd12940e1afacfc1723b
Harshavardhan Edke <harsh.edke@gmail.com>
2013-01-19 18:58:46 +0530

    webtool.sql 19-01-2013

--- webtool.sql
+++ webtool.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 18, 2013 at 04:42 AM
+-- Generation Time: Jan 19, 2013 at 01:27 PM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.6
 
@@ -22,6 +22,24 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` bigint(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `parent_id` bigint(20) NOT NULL,
+  PRIMARY KEY  (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `file`
 --
 
@@ -32,14 +50,76 @@ CREATE TABLE `file` (
   `file_name` varchar(50) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `file`
 --
 
 INSERT INTO `file` (`id`, `user_id`, `file_path`, `file_name`) VALUES
-(1, 7, '../videos/', '1.mp4');
+(8, 1, '../images/', '8.png'),
+(9, 1, '../videos/', '9.mp4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu_id` bigint(20) NOT NULL,
+  `page_id` bigint(20) NOT NULL,
+  `website_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `parent_menu` bigint(20) NOT NULL,
+  PRIMARY KEY  (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seo_settings`
+--
+
+CREATE TABLE `seo_settings` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `website_id` bigint(20) NOT NULL,
+  `keyword` varchar(100) NOT NULL,
+  `meta_tag` varchar(100) NOT NULL,
+  `site_title` varchar(100) NOT NULL,
+  `meta_description` varchar(500) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `seo_settings`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `template`
+--
+
+CREATE TABLE `template` (
+  `template_id` bigint(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `category_id` bigint(20) NOT NULL,
+  PRIMARY KEY  (`template_id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `template`
+--
+
 
 -- --------------------------------------------------------
 
@@ -53,20 +133,72 @@ CREATE TABLE `user` (
   `email` varchar(256) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL default 'inactive',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`) VALUES
-(3, 'Harsh Edke', 'harsh@gmail.com', 'harshedke', '793ceb6f176b4f6d74f79af5d226a37a'),
-(4, 'Sachin Thinge', 'sachin@gmail.cim', 'sachin', '15285722f9def45c091725aee9c387cb'),
-(5, 'harsh edke', 'hae@gg.ccc', 'asd', 'a3dcb4d229de6fde0db5686dee47145d'),
-(7, 'test', 'test@test.com', 'test', 'cc03e747a6afbbcbf8be7668acfebee5');
+INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`, `status`) VALUES
+(1, 'Harsh Edke', 'harsh.edke@gmail.com', 'harshe', '49db7687173513257476390f4dcadab9', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `addressline1` varchar(500) NOT NULL,
+  `addressline2` varchar(500) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `pincode` varchar(100) NOT NULL,
+  `timezone` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL default 'inactive',
+  `oauth_uid` varchar(200) NOT NULL,
+  `provider` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `email_id` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`id`, `name`, `username`, `password`, `phone`, `email`, `addressline1`, `addressline2`, `country`, `state`, `city`, `pincode`, `timezone`, `status`, `oauth_uid`, `provider`) VALUES
+(1, 'Harsh Edke', 'harshe', '49db7687173513257476390f4dcadab9', '', 'harsh.edke@gmail.com', '', '', '', '', '', '', '', 'active', '', ''),
+(2, 'Sachin Thonge', '', '', '', 'sachinthonge11@gmail.com', '', '', '', '', '', '', '', 'inactive', '100001938682824', 'facebook'),
+(3, 'Harshavardhan Edke', '', '', '', 'harashvaradhan@gmail.com', '', '', '', '', '', '', '', 'inactive', '100004799434107', 'facebook');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `website`
+--
+
+CREATE TABLE `website` (
+  `website_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `website_name` varchar(100) NOT NULL,
+  PRIMARY KEY  (`website_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `website`
+--
+
 
 --
 -- Constraints for dumped tables
