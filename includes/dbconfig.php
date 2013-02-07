26d6207f5c47fa8287a7f7e33c5903c44a7e69e6
harshedke <harsh.edke@gmail.com>
2013-02-07 13:01:43 +0530

    changes from ubuntu

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -3,6 +3,6 @@ define('HOST', "localhost");
 define('DB_SERVER',"localhost");
 define('DB_DATABASE',"webtool");
 define('DB_USER',"root");
-define('DB_PASSWORD',"");
+define('DB_PASSWORD',"root123");
 define('PAGERECORD', 10);
 ?>
--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-include_once("dbConfig.php");
+include_once("dbconfig.php");
 
 ///File that allows access to Database
 class DBConnection
